f1fc5abf50dfcf286a9a5d1710efd1f9e4bcf262
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-22 16:42:02 +0100

    feat: get trains query added

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,6 +10,11 @@ class PageController extends Controller
     // Ritorno la view home
     public function index()
     {
-        return view('home');
+        // Prelevo i treni che devono partire da oggi in poi e li ordino per orario di partenza
+        $trains = Train::where('departure_time', '>=', Carbon::today())
+            ->orderBy('departure_time')
+            ->get();
+        
+        return view('home', compact('trains'));
     }
 }
