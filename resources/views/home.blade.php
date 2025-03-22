f77e94f67f6532fb21741c082530b70cd05ff592
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-22 16:24:53 +0100

    feat: main section created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,7 @@
-<h1>Hello</h1>
+@extends('Layouts.master')
+
+@section('content')
+    <div class="container">
+        <h1 class="uppercase text-center pt-4 fw-bolder">Departing Trains</h1>
+    </div>
+@endsection
